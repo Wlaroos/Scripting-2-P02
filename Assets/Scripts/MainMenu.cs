829f2af7f9608b44cbc8e274221965d8815a6172
Mikey <mikeybess27@gmail.com>
2022-10-25 01:54:49 -0500

    Quit Button, Folder Organization

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -11,4 +11,14 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void Settings()
+    {
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
 }
