435ed02ae60eafa8f6f29c93e365693fb6302a1d
Mikey <mikeybess27@gmail.com>
2022-11-07 03:26:04 -0600

    Comments and organization

--- Assets/Scripts/UI/UILinePointsTest.cs
+++ Assets/Scripts/UI/UILinePointsTest.cs
@@ -6,32 +6,42 @@ using UnityEngine.UI;
 
 public class UILinePointsTest : MonoBehaviour
 {
+    // Array of Line Objects
     [SerializeField] UILineRenderer[] _lines;
-    List<GameObject> _images = new List<GameObject>();
-    List<Vector2> _pointzzz = new List<Vector2>();
 
-    int _count;
+    // List of Point Objects
+    List<GameObject> _images = new List<GameObject>();
+    // List of points in ALL Line Objects
+    List<Vector2> _localPoints = new List<Vector2>();
+
+    int _totalPointCount;
     float _imageWidth = 50f;
 
-    RectTransform recttt;
+    RectTransform _rectTransRef;
 
     private void Awake()
     {
-        recttt = GetComponent<RectTransform>();
+        _rectTransRef = GetComponent<RectTransform>();
 
+        // For every line object...
         for (int i = 0; i < _lines.Length; i++)
         {
+            // Set References
             _lines[i] = transform.GetChild(i).GetComponent<UILineRenderer>();
-            _count += _lines[i]._points.Count;
+            // Add to total point count from each line's point count
+            _totalPointCount += _lines[i]._points.Count;
+            // All width values should be the same, so grab from whichever
             _imageWidth = _lines[0]._imageWidth;
 
+            // Add every Vector2 from each line object to the Vector2 List
             for (int j = 0; j < _lines[i]._points.Count; j++)
             {
-                _pointzzz.Add(_lines[i]._points[j]);
+                _localPoints.Add(_lines[i]._points[j]);
             }
         }
 
-        for (int j = 0; j < _count; j++)
+        // Create and set default values for every point needed -- Also, add each gameobject to the _images List
+        for (int j = 0; j < _totalPointCount; j++)
         {
             _images.Add(new GameObject("Point", typeof(RectTransform), typeof(CanvasRenderer), typeof(Image)));
 
@@ -49,6 +59,7 @@ public class UILinePointsTest : MonoBehaviour
         }
     }
 
+    // Gives the LineRenderer script time to setup
     private void Start()
     {
         Invoke("Delay", .01f);
@@ -56,27 +67,36 @@ public class UILinePointsTest : MonoBehaviour
 
     private void Delay()
     {
+        // For each game object created, set the transform values to the vertex points of the line
+        // This is done through the MoveImages method -- Have to subtract half of the rect transform's size becuase of the way the achors are set up
+        int i = 1;
         for (int j = 0; j < _images.Count - 1; j++)
         {
-            Vector2 point = _pointzzz[j];
-            Vector2 point2 = _pointzzz[j + 1];
+            Vector2 point = _localPoints[j];
+            Vector2 point2 = _localPoints[j + 1];
 
-            _images[j].transform.localPosition = _lines[0].MoveImages(point) - new Vector3(recttt.sizeDelta.x/2, recttt.sizeDelta.y/2, 0);
-            if (j + 1 == _lines[0]._points.Count - 1 || j + 1 == _lines[0]._points.Count*2 - 1)
+            _images[j].transform.localPosition = _lines[0].MoveImages(point) - new Vector3(_rectTransRef.sizeDelta.x/2, _rectTransRef.sizeDelta.y/2, 0);
+
+            // This catches the final points in each line's list of points
+            if (j + 1 == _lines[0]._points.Count*i - 1)
             {
-                _images[j + 1].transform.localPosition = _lines[0].MoveImages(point2) - new Vector3(recttt.sizeDelta.x/2,recttt.sizeDelta.y/2,0);
+                _images[j + 1].transform.localPosition = _lines[0].MoveImages(point2) - new Vector3(_rectTransRef.sizeDelta.x/2,_rectTransRef.sizeDelta.y/2,0);
+                i++;
             }
         }
 
+        // Remove duplicate images
         RemoveDuplicatesFrom(_images);
     }
 
+    // Checking list for duplicates
     private void RemoveDuplicatesFrom(List<GameObject> collection)
     {
         for (int i = 0; i < collection.Count; i++)
         {
             for (int j = i + 1; j < collection.Count; j++)
             {
+                // If duplicate is found, destroy and remove from list
                 if (!Match(collection[i], collection[j]))
                     continue;
 
@@ -87,11 +107,10 @@ public class UILinePointsTest : MonoBehaviour
         }
     }
 
+    // Test to see if transforms match
     private bool Match(GameObject object1, GameObject object2)
     {
-        return
-            object1.transform.position.x == object2.transform.position.x &&
-            object1.transform.position.y == object2.transform.position.y;
+        return object1.transform.position.x == object2.transform.position.x && object1.transform.position.y == object2.transform.position.y;
     }
 
     private void FixedUpdate()
