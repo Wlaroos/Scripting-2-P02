8f539a8a26c4ba944762235f96fa67d305a95cf1
Mikey <mikeybess27@gmail.com>
2022-11-07 21:40:49 -0600

    SweetSpot Minigame

---
+++ Assets/Scripts/UI/Minigame/SweetSpot.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SweetSpot : MonoBehaviour
+{
+
+    [SerializeField] Slider _sliderRef;
+    [SerializeField] float sliderSpeed = 100f;
+
+    bool isSliding = false;
+    bool isDirectionUp = true;
+    float amtSlider = 0.0f;
+
+    private void OnEnable()
+    {
+        StartSlider();
+    }
+
+    private void OnDisable()
+    {
+        isSliding = false;
+        isDirectionUp = true;
+        amtSlider = 0.0f;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            EndSlider();
+        }
+
+        if (isSliding)
+        {
+            SliderActive();
+        }
+    }
+
+    void SliderActive()
+    {
+        if(isDirectionUp)
+        {
+            amtSlider += Time.deltaTime * sliderSpeed;
+            if(amtSlider > 100)
+            {
+                isDirectionUp = false;
+                amtSlider = 100f;
+            }
+        }
+        else
+        {
+            amtSlider -= Time.deltaTime * sliderSpeed;
+            if (amtSlider < 0)
+            {
+                isDirectionUp = true;
+                amtSlider = 0f;
+            }
+        }
+
+        _sliderRef.value = amtSlider;
+
+    }
+
+    public void StartSlider()
+    {
+        isSliding = true;
+        amtSlider = 0.0f;
+        isDirectionUp = true;
+    }
+
+    public void EndSlider()
+    {
+        isSliding = false;
+        Debug.Log(amtSlider);
+    }
+}
